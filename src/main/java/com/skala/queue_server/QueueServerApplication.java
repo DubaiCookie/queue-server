d1cd79df9a71273cab6675859b19e6722c992391
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2026-02-08 23:38:49 +0900

    init : 레포지토리 생성

---
+++ src/main/java/com/skala/queue_server/QueueServerApplication.java
@@ -0,0 +1,13 @@
+package com.skala.queue_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skala/queue_server/QueueServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skala.queue_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
